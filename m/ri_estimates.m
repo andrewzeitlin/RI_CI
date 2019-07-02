442f40658e1438d6f78534a9edbcc719bd547173
Andrew Zeitlin <andrew.zeitlin@gmail.com>
2019-07-02 14:53:13 -0400

    RI based on adjusted outcomes

--- m/ri_estimates.m
+++ m/ri_estimates.m
@@ -27,23 +27,11 @@ function [pvalue TEST0 test1 y0 ] = ri_estimates(DATA,outcome,txvars,tau0, model
 	RunParallel = options.Results.RunParallel; 
 
 	%  Run DGP in reverse to get y0
-	y0 = table2array(DATA(:,outcome)) - table2array(DATA(:,txvars)) * tau0 ; 
+	t1 = table2array(DATA(:,txvars)); % actual assignment vector(s)
+	y0 = table2array(DATA(:,outcome)) - t1 * tau0 ; 
 	y0(~isnan(y0)) = min(max(y0(~isnan(y0)), Support(1)),Support(2)) ;  % impose support on non-missing values of y0
 
-	%  If model is ks and TestValue has not been specified, estimate KS statistic on the hypothesized y0
-	if strcmp(model,'ks') && length(TestValue) == 0
-		if length(txvars) > 1 
-			tx = table2array(DATA(:,txvars(find(strcmp(txvars,TheTx)))));
-		else 
-			tx = table2array(DATA(:,txvars)); 
-		end
-		[~,~,TestValue] = kstest2(y0(tx==1),y0(tx==0));
-	end
-
-	%  Now, loop over feasible randomizations, impose treatment effect, re-estimate, and extract test statistic
-
 	% For speed, and potential parallelizability: don't swap treatments into data table, but run this as a matrix. 
-	%  Extract only once..
 	if strcmp(model,'lm')
 		x = [ ... %ones(size(DATA,1),1) , 
 			table2array(DATA(:,Controls)) ...
@@ -51,24 +39,40 @@ function [pvalue TEST0 test1 y0 ] = ri_estimates(DATA,outcome,txvars,tau0, model
 	else 
 		x = []; % creating empty matrix in this case just so it can be passed to the getTestStat function without worrying about model-specific cases.
 	end 
-	
+
+	%  Estimate test statistic on the hypothesized y0 using the *actual* assignment
+	if length(TestValue) == 0
+		if strcmp(model, 'rereg')
+			TestValue = getTestStat(y0,txvars,TheTx,t1,model,TestType,'Controls',Controls,'Support',Support,'x',x,'DATA',DATA); 
+		else
+			TestValue = getTestStat(y0,txvars,TheTx,t1,model,TestType,'Controls',Controls,'Support',Support,'x',x);  
+		end
+	end
+
+	%  Now, loop over feasible randomizations, impose treatment effect, re-estimate, and extract test statistic
 	if RunParallel
 		parfor pp = 1 : P 
+			%  Draw treatment 
+			t0 = permute(T0(:,pp,:),[1 3 2]);  % accommodates possibliity of multiple treatment variables
+
 			if strcmp(model, 'rereg')
-				testStat = getTestStat(y0,tau0,txvars,TheTx,model,TestType,T0,pp,'Controls',Controls,'Support',Support,'x',x,'DATA',DATA); 
+				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x,'DATA',DATA); 
 			else
-				testStat = getTestStat(y0,tau0,txvars,TheTx,model,TestType,T0,pp,'Controls',Controls,'Support',Support,'x',x);  
+				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x);  
 			end
 			TEST0(pp,:) = testStat;
 		end
 	else 
 		if ShowWaitBar, hh = waitbar(0, WaitMessage); end
 		for pp = 1 : P
+			%  Draw treatment 
+			t0 = permute(T0(:,pp,:),[1 3 2]);  % accommodates possibliity of multiple treatment variables
+
 			% Using model-specific calls for rereg to avoid creating extra copies of the dataset otherwise 
 			if strcmp(model, 'rereg')
-				testStat = getTestStat(y0,tau0,txvars,TheTx,model,TestType,T0,pp,'Controls',Controls,'Support',Support,'x',x,'DATA',DATA); 
+				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x,'DATA',DATA); 
 			else
-				testStat = getTestStat(y0,tau0,txvars,TheTx,model,TestType,T0,pp,'Controls',Controls,'Support',Support,'x',x);  
+				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x);  
 			end
 			TEST0(pp,:) = testStat;
 			if ShowWaitBar, waitbar(pp/P); end 
@@ -90,7 +94,7 @@ function [pvalue TEST0 test1 y0 ] = ri_estimates(DATA,outcome,txvars,tau0, model
 end
 
 
-function testStat = getTestStat(y0,tau0,txvars,TheTx,model,TestType,T0,pp,varargin)
+function testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,varargin)
 	%  Parse input 
 	parameters = inputParser ; 
 	addOptional(parameters,'x',[]); 
@@ -105,33 +109,23 @@ function testStat = getTestStat(y0,tau0,txvars,TheTx,model,TestType,T0,pp,vararg
 	DATA = parameters.Results.DATA;
 	groupvar = parameters.Results.groupvar; 	
 
-	%  For KS stat, RI based on y0
 	if strcmp(model, 'ks')
 		if length(txvars) > 1 
-			tx = T0(:,pp,find(strcmp(txvars,TheTx)))
 			%  TODO:  For the KS test, when multiple treatment variables, need to residualize outcome to remove this as a potential source of bias here.
+			tx = t0(find(strcmp(txvars,TheTx)));
+			[~,~,testStat ] = kstest2(y0(tx==1),y0(tx==0)) ; % two-sample KS stat
 		else 
-			tx = (T0(:,pp)); 
+			[~,~,testStat ] = kstest2(y0(t0==1),y0(t0==0)) ; % two-sample KS stat
 		end
-		[~,~,testStat ] = kstest2(y0(tx==1),y0(tx==0)) ; % two-sample KS stat
-	else 
-		%  Impose hypothesized DGP
-		t0 = permute(T0(:,pp,:),[1 3 2]);  % accommodates possibliity of multiple treatment variables
-
-		ystar = y0 + t0 * tau0 ; 
-		ystar(~isnan(ystar)) = min(Support(2),max(Support(1), ystar(~isnan(ystar))));
-
-		%  Estimate model and collect test statistic
-		if strcmp(model,'lm')
-			lm = fitlm([t0 , x ], ystar) ; % 
-			testStat = table2array(lm.Coefficients(1+find(strcmp(txvars,TheTx)), [TestType]));
-		elseif strcmp(model,'re') 
-			DATA.ystar = ystar ; % rereg() syntax requires this to be part of the table.
-			DATA(:,txvars) = array2table(t0) ; 
-			result = rereg(DATA,{'ystar'},[txvars Controls],groupvar);
-			testStat = table2array(result(find(strcmp(txvars,TheTx)), TestType));
-		else
-			error('Must specify a valid model')
-		end
+	elseif strcmp(model,'lm')
+		lm = fitlm([t0 , x ], y0) ; % 
+		testStat = table2array(lm.Coefficients(1+find(strcmp(txvars,TheTx)), [TestType]));
+	elseif strcmp(model,'re') 
+		DATA.y0 = y0 ; % rereg() syntax requires this to be part of the table.
+		DATA(:,txvars) = array2table(t0) ; 
+		result = rereg(DATA,{'y0'},[txvars Controls],groupvar);
+		testStat = table2array(result(find(strcmp(txvars,TheTx)), TestType));
+	else
+		error('Must specify a valid model')
 	end
 end
