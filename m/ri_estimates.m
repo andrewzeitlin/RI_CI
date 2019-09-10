2b8d32e14878ce99b8d949f26e01e0cc92531961
Andrew Zeitlin <andrew.zeitlin@gmail.com>
2019-09-10 05:06:42 -0400

    use fitlmematrix in ri_estimates

--- m/ri_estimates.m
+++ m/ri_estimates.m
@@ -17,7 +17,7 @@ function [pvalue TEST0 test1 y0 ] = ri_estimates(DATA,outcome,txvars,tau0, model
 	addOptional(options,'formula',{});
 
 	parse(options,varargin{:}); 
-	groupvar = options.Results.GroupVar; 
+	GroupVar = options.Results.GroupVar; 
 	TheTx = options.Results.TheTx ;
 	TestSide = options.Results.TestSide; 
 	TestValue = options.Results.TestValue; 
@@ -35,71 +35,36 @@ function [pvalue TEST0 test1 y0 ] = ri_estimates(DATA,outcome,txvars,tau0, model
 	y0(~isnan(y0)) = min(max(y0(~isnan(y0)), Support(1)),Support(2)) ;  % impose support on non-missing values of y0
 
 	%%  SETUP 
-	if strcmp(model, 'lme') 	%  LME:  add y0 to data table, interpret into formula 
-		DATA.y0 = y0 ; 
-		if ~strcmp(formula,'')
-			%  Translate estimmation parameters into formula for lme 
-			rhs = [txvars Controls ] ; 
-			formula = [ outcome{1} ' ~ 1 ' ] ; 
-			for k = 1:length(rhs) 
-				formula = [ formula ' + ' rhs{k}] ; 
-			end	
-			for k = 1:length(groupvar)
-				formula = [ formula ' + (1 | ' groupvar{k} ' ) '] ; 
-			end
-		end
-	elseif strcmp(model,'lm') || strcmp(model,'re') 	% Otherwise, for speed, and potential parallelizability: don't swap treatments into data table, but run this as a matrix. 
+	if strcmp(model,'lm') || strcmp(model,'re') || strcmp(model,'lme') 	% For speed, and potential parallelizability: don't swap treatments into data table, but run this as a matrix. 
 		x = table2array(DATA(:,Controls)); 
 		if strcmp(model,'re')
-			g = table2array(DATA(:,groupvar));
+			g = table2array(DATA(:,GroupVar));
 		end
 	else 
 		x = []; % creating empty matrix in this case just so it can be passed to the getTestStat function without worrying about model-specific cases.
+		g = [] ; % as above
 	end 
 
-	%5  EXTRACT TEST STAT
+	%%  EXTRACT TEST STAT
 	%  Estimate test statistic on the hypothesized y0 using the *actual* assignment
 	if length(TestValue) == 0
-		if strcmp(model, 'lme') % function call is different for this one because it operates only on tables
-			TestValue = lmeTestStat(DATA,formula,TheTx,TestType)
-		elseif strcmp(model, 're') % separating this because it requires an extra argument (the group variable) to be passed as an array.
-			TestValue = getTestStat(y0,txvars,TheTx,t1,model,TestType,'Controls',Controls,'Support',Support,'x',x,'g',g); 
-		else
-			TestValue = getTestStat(y0,txvars,TheTx,t1,model,TestType,'Controls',Controls,'Support',Support,'x',x);  
-		end
+		TestValue = getTestStat(y0,txvars,TheTx,t1,model,TestType,'Controls',Controls,'Support',Support,'x',x,'g',g,'GroupVar',GroupVar);  
 	end
 
 	%  Now, loop over feasible randomizations, impose treatment effect, re-estimate, and extract test statistic
 	if RunParallel
 		parfor pp = 1 : P 
-			%  Draw treatment 
+			%  Draw treatment permutation and extract test statistic
 			t0 = permute(T0(:,pp,:),[1 3 2]);  % accommodates possibliity of multiple treatment variables
-
-			if strcmp(model, 're')
-				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x,'g',g); 
-			elseif strcmp(model, 'lme')
-				% DATA(:,txvars) = array2table(t0) ; % Replace treatment variables in memory with values from this permutation
-				testStat = lmeTestStat(DATA,formula,txvars,t0,TheTx,TestType) ; % estimate and extract test statistic 
-			else
-				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x);  
-			end
+			testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x,'g',g,'GroupVar',GroupVar); 
 			TEST0(pp,:) = testStat;
 		end
 	else 
 		if ShowWaitBar, hh = waitbar(0, WaitMessage); end
 		for pp = 1 : P
-			%  Draw treatment 
+			%  Draw treatment permutation and extract test statistic
 			t0 = permute(T0(:,pp,:),[1 3 2]);  % accommodates possibliity of multiple treatment variables
-
-			% Using model-specific calls for rereg to avoid creating extra copies of the dataset otherwise 
-			if strcmp(model, 're')
-				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x,'g',g); 
-			elseif strcmp(model, 'lme')
-				% DATA(:,txvars) = array2table(t0) ; % Replace treatment variables in memory with values from this permutation
-				testStat = lmeTestStat(DATA,formula,txvars,t0,TheTx,TestType) ; % estimate and extract test statistic 
-			else
-				testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x);  
-			end
+			testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,'Controls',Controls,'Support',Support,'x',x,'g',g,'GroupVar',GroupVar); 
 			TEST0(pp,:) = testStat;
 			if ShowWaitBar, waitbar(pp/P); end 
 		end
@@ -118,20 +83,6 @@ function [pvalue TEST0 test1 y0 ] = ri_estimates(DATA,outcome,txvars,tau0, model
 	end
 end
 
-function testStat = lmeTestStat(DATA,formula,txvars,t0,TheTx,TestType)
-	DATA(:,txvars) = array2table(t0) ; % substitute this permutation into DATA table as the assignment vector
-	lme = fitlme(DATA,formula); 
-	[~,~,stats] = fixedEffects(lme) ;  % extract coefficients ('Estimate') and t-stats ('tStat') . 
-	%  Check for lme's tendency to rename variables with _1 
-	indicators = find(contains(stats.Name,'_1')) ; 
-	for ii = indicators 
-		stats.Name(ii) = strrep(stats.Name(ii),'_1','');
-	end
-	stats.Properties.RowNames = stats.Name; % add row names 
-	testStat = stats(TheTx,TestType);
-end
-
-
 function testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,varargin)
 	%  Parse input 
 	parameters = inputParser ; 
@@ -139,11 +90,14 @@ function testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,varargin)
 	addOptional(parameters,'Controls',{});
 	addOptional(parameters,'Support',[-inf,inf]);
 	addOptional(parameters,'g',[]);
+	addOptional(parameters,'GroupVar',{}); 
 	parse(parameters,varargin{:}); 
 	x = parameters.Results.x;
 	Controls = parameters.Results.Controls;
+	GroupVar = parameters.Results.GroupVar;
 	Support = parameters.Results.Support; 
 	g = parameters.Results.g; 	
+
 
 	if strcmp(model, 'ks')
 		if length(txvars) > 1 
@@ -155,6 +109,24 @@ function testStat = getTestStat(y0,txvars,TheTx,t0,model,TestType,varargin)
 	elseif strcmp(model,'lm')
 		lm = fitlm([t0 , x ], y0) ; % 
 		testStat = table2array(lm.Coefficients(1+find(strcmp(txvars,TheTx)), [TestType]));
+	elseif strcmp(model,'lme') 
+		lme = fitlmematrix( ...
+			[ones(size(DATA,1),1), t0, x] ...  % FE design matrix
+			, y0 ...  % outcome
+			, ones(size(DATA,1),1) ... % RE design matrix
+			, g ... % grouping variable(s) 
+			, 'FixedEffectPredictors', [ 'Intercept' txvars Controls ] ...
+			, 'RandomEffectPredictors', GroupVar ...
+			) ; 
+		[~,~,stats] = fixedEffects(lme) ;  % extract coefficients ('Estimate') and t-stats ('tStat') . 
+		%  Check for lme's tendency to rename variables with _1 %%  NOT NECESSARY WITH MATRIX VERSION AND MANUAL NAMING OF VARIABLES?
+		% indicators = find(contains(stats.Name,'_1')) ; 
+		% for ii = indicators 
+		% 	stats.Name(ii) = strrep(stats.Name(ii),'_1','');
+		% end
+		stats.Properties.RowNames = stats.Name; % add row names 
+		testStat = stats(TheTx,TestType);
+
 	elseif strcmp(model,'re') 
 		result = rereg_array(y0,[t0, x],g,[txvars Controls]) ; 
 		testStat = table2array(result(TheTx, TestType));
